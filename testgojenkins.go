b6bc18059e7843274cd6857e46c2506be83f5463
Shilpi Sinha <shilpi.sinha@stonetusker.com>
2025-11-04 19:35:32 +0530

    commit with Go app and Dockerfile

--- testgojenkins.go
+++ testgojenkins.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, Jenkins & Docker..from Stonetusker!!")
+ 
+	fmt.Println("Hello, Jenkins & Docker..from Stonetusker!!")
+
+	fmt.Println("Build & Deployment pipeline executed successfully !!")
+
 }
